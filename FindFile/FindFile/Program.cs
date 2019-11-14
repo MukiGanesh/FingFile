7ae9f1c5f704805c72fb6bab923ef20282ab2d3c
MukiGanesh <48015408+MukiGanesh@users.noreply.github.com>
2019-11-14 16:20:08 +0530

    Initial setup

---
+++ FindFile/FindFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FindFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nEnter FileName: ");
+            var fileName = Console.ReadLine();
+            Console.WriteLine($"\nFilePath - { fileName}");
+            Console.Write("\n Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
